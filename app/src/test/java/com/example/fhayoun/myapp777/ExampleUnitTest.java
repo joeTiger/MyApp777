6b3061d40a8466b4cc54f9bf3210582b8e943d2b
Joe Tiger <joetiger05@gmail.com>
2016-05-10 10:08:48 +0300

    add div test

--- app/src/test/java/com/example/fhayoun/myapp777/ExampleUnitTest.java
+++ app/src/test/java/com/example/fhayoun/myapp777/ExampleUnitTest.java
@@ -12,4 +12,9 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void div_isCorrect() throws Exception {
+        assertEquals(1, 2 / 2);
+    }
 }
